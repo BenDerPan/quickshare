ef0c3d3fcc279bcac5b6d1181c1d17e26c2df5bd
hexxa <hexxa@outlook.com>
2018-09-19 22:23:45 +0800

    feat(ratelimiter): snapshot

--- server/libs/limiter/mem_limiter.go
+++ server/libs/limiter/mem_limiter.go
@@ -1,239 +1,149 @@
 package limiter
 
 import (
-	// "sync"
+	"sync"
 	"time"
+	"errors"
 )
 
-func now() int32 {
-	return int32(time.Now().Unix())
+const accessOk = 0
+const accessNoToken = 1
+const accessShouldReset = 2
+
+var errLimiterIsFull = errors.New("Limiter reachs its capacity")
+
+func now() int64 {
+	return time.Now().Unix()
 }
 
-func after(duration int32) int32 {
-	return int32(time.Now().Unix()) + duration
+func after(duration int64) int64 {
+	return now() + duration
 }
-
-// func afterCyc(cyc int32) int32 {
-// 	return int32(time.Now().Unix()) + cyc
-// }
-
-// func afterTtl(ttl int32) int32 {
-// 	return int32(time.Now().Unix()) + ttl
-// }
 
 type Bucket struct {
-	ResetAt int32
-	Tokens  int16
+	ResetAt int64
+	Tokens  int
 }
 
-func NewBucket(resetCyc int32, cap int16) *Bucket {
+func NewBucket(resetCyc int64, tokens int) *Bucket {
+	if resetCyc <= 0 || tokens < 0 {
+		return nil
+	}
+
 	return &Bucket{
 		ResetAt: after(resetCyc),
-		Tokens:  cap,
+		Tokens:  tokens,
 	}
 }
 
-func (b *Bucket) Access(resetCyc int32, add int16) bool {
+func (b *Bucket) Access() int {
 	now := now()
 
-	if b.ResetAt <= now {
-		b.ResetAt = after(resetCyc)
-		b.Tokens = add - 1
-		return true
+	if b.ResetAt > now && b.Tokens == 0 {
+		return accessNoToken
 	} else if b.ResetAt > now && b.Tokens > 0 {
-		b.Tokens--
-		return true
+		return accessOk
 	}
-	return false
+	return accessShouldReset
 }
 
+func (b *Bucket) Reset(resetCyc int64, resetTokens int) {
+	b.ResetAt = after(resetCyc)
+	b.Tokens = resetTokens - 1
+}
 
-// type Item struct {
-// 	Expired int32
-// 	Buckets map[int16]*Bucket
-// }
+func (b *Bucket) Minus() {
+	b.Tokens--
+}
 
-// func NewItem(ttl int32) *Item {
-// 	return &Item{
-// 		Expired: afterTtl(ttl),
-// 		Buckets: make(map[int16]*Bucket),
-// 	}
-// }
+type MemLimiter struct {
+	buckets   map[string]*Bucket
+	cap       int
+	mux       sync.RWMutex
+	rstCyc    int64
+	rstTokens int
+}
 
-// type RateLimiter struct {
-// 	items      map[string]*Item
-// 	bucketCap  int16
-// 	customCaps map[int16]int16
-// 	cap        int64
-// 	cyc        int32 // how much time, item autoclean will be executed, bucket will be refreshed
-// 	ttl        int32 // how much time, item will be expired(but not cleaned)
-// 	mux        sync.RWMutex
-// 	snapshot   map[string]map[int16]*Bucket
-// }
+func New(cap int, rstCyc int64, rstTokens int) *MemLimiter {
+	if cap <= 0 || rstCyc <= 0 || rstTokens < 0 {
+		return nil
+	}
 
-// func NewRateLimiter(cap int64, ttl int32, cyc int32, bucketCap int16, customCaps map[int16]int16) Limiter {
-// 	if cap < 1 || ttl < 1 || cyc < 1 || bucketCap < 1 {
-// 		panic("cap | bucketCap | ttl | cycle cant be less than 1")
-// 	}
+	return &MemLimiter{
+		cap:       cap,
+		buckets:   make(map[string]*Bucket, 0),
+		rstCyc:    rstCyc,
+		rstTokens: rstTokens,
+	}
+}
 
-// 	limiter := &RateLimiter{
-// 		items:      make(map[string]*Item, cap),
-// 		bucketCap:  bucketCap,
-// 		customCaps: customCaps,
-// 		cap:        cap,
-// 		ttl:        ttl,
-// 		cyc:        cyc,
-// 	}
+func (l *MemLimiter) Access(id string) (bool, error) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
 
-// 	go limiter.autoClean()
+	bucket, found := l.buckets[id]
+	if !found {
+		if len(l.buckets) >= l.cap {
+			return false, errLimiterIsFull
+		}
+		l.buckets[id] = NewBucket(l.rstCyc, l.rstTokens)
+		return true, nil
+	}
 
-// 	return limiter
-// }
+	switch bucket.Access() {
+	case accessNoToken:
+		return false, nil
+	case accessOk:
+		bucket.Minus()
+		return true, nil
+	default:
+		bucket.Reset(l.rstCyc, l.rstTokens)
+		return true, nil
+	}
+}
 
-// func (limiter *RateLimiter) getBucketCap(opId int16) int16 {
-// 	bucketCap, existed := limiter.customCaps[opId]
-// 	if !existed {
-// 		return limiter.bucketCap
-// 	}
-// 	return bucketCap
-// }
+func (l *MemLimiter) GetSize() int {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	return len(l.buckets)
+}
 
-// func (limiter *RateLimiter) Access(itemId string, opId int16) bool {
-// 	limiter.mux.Lock()
-// 	defer limiter.mux.Unlock()
+func (l *MemLimiter) SetCap(cap int) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	if cap > l.cap {
+		l.cap = cap
+	}
+}
 
-// 	item, itemExisted := limiter.items[itemId]
-// 	if !itemExisted {
-// 		if int64(len(limiter.items)) >= limiter.cap {
-// 			return false
-// 		}
+func (l *MemLimiter) GetCap() int {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	return l.cap
+}
 
-// 		limiter.items[itemId] = NewItem(limiter.ttl)
-// 		limiter.items[itemId].Buckets[opId] = NewBucket(limiter.cyc, limiter.getBucketCap(opId)-1)
-// 		return true
-// 	}
+func (l *MemLimiter) SetResetCyc(rstCyc int64) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	l.rstCyc = rstCyc
+}
 
-// 	bucket, bucketExisted := item.Buckets[opId]
-// 	if !bucketExisted {
-// 		item.Buckets[opId] = NewBucket(limiter.cyc, limiter.getBucketCap(opId)-1)
-// 		return true
-// 	}
+func (l *MemLimiter) GetResetCyc() int64 {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	return l.rstCyc
+}
 
-// 	if bucket.Refresh > now() {
-// 		if bucket.Tokens > 0 {
-// 			bucket.Tokens--
-// 			return true
-// 		}
-// 		return false
-// 	}
+func (l *MemLimiter) SetRstTokens(rstTokens int) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	l.rstTokens = rstTokens
+}
 
-// 	bucket.Refresh = afterCyc(limiter.cyc)
-// 	bucket.Tokens = limiter.getBucketCap(opId) - 1
-// 	return true
-// }
+func (l *MemLimiter) GetRstTokens() int {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	return l.rstTokens
+}
 
-// func (limiter *RateLimiter) GetCap() int64 {
-// 	return limiter.cap
-// }
-
-// func (limiter *RateLimiter) GetSize() int64 {
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-// 	return int64(len(limiter.items))
-// }
-
-// func (limiter *RateLimiter) ExpandCap(cap int64) bool {
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-
-// 	if cap <= int64(len(limiter.items)) {
-// 		return false
-// 	}
-
-// 	limiter.cap = cap
-// 	return true
-// }
-
-// func (limiter *RateLimiter) GetTTL() int32 {
-// 	return limiter.ttl
-// }
-
-// func (limiter *RateLimiter) UpdateTTL(ttl int32) bool {
-// 	if ttl < 1 {
-// 		return false
-// 	}
-
-// 	limiter.ttl = ttl
-// 	return true
-// }
-
-// func (limiter *RateLimiter) GetCyc() int32 {
-// 	return limiter.cyc
-// }
-
-// func (limiter *RateLimiter) UpdateCyc(cyc int32) bool {
-// 	if limiter.cyc < 1 {
-// 		return false
-// 	}
-
-// 	limiter.cyc = cyc
-// 	return true
-// }
-
-// func (limiter *RateLimiter) Snapshot() map[string]map[int16]*Bucket {
-// 	return limiter.snapshot
-// }
-
-// func (limiter *RateLimiter) autoClean() {
-// 	for {
-// 		if limiter.cyc == 0 {
-// 			break
-// 		}
-// 		time.Sleep(time.Duration(int64(limiter.cyc) * 1000000000))
-// 		limiter.clean()
-// 	}
-// }
-
-// // clean may add affect other operations, do frequently?
-// func (limiter *RateLimiter) clean() {
-// 	limiter.snapshot = make(map[string]map[int16]*Bucket)
-// 	now := now()
-
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-// 	for key, item := range limiter.items {
-// 		if item.Expired <= now {
-// 			delete(limiter.items, key)
-// 		} else {
-// 			limiter.snapshot[key] = item.Buckets
-// 		}
-// 	}
-// }
-
-// // Only for test
-// func (limiter *RateLimiter) exist(id string) bool {
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-
-// 	_, existed := limiter.items[id]
-// 	return existed
-// }
-
-// // Only for test
-// func (limiter *RateLimiter) truncate() {
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-
-// 	for key, _ := range limiter.items {
-// 		delete(limiter.items, key)
-// 	}
-// }
-
-// // Only for test
-// func (limiter *RateLimiter) get(id string) (*Item, bool) {
-// 	limiter.mux.RLock()
-// 	defer limiter.mux.RUnlock()
-
-// 	item, existed := limiter.items[id]
-// 	return item, existed
-// }
+// auto clean func
